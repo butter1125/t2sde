e2adedfe058e8f1784fed82c89eef6b09b5e8f76
butter1125 <butter011125@gmail.com>
2025-03-14 20:21:37 -0700

    Update config.fish

--- package/shells/fish/config.fish
+++ package/shells/fish/config.fish
@@ -169,8 +169,3 @@ alias jctl 'journalctl -p 3 -xb'
 
 # Recent installed packages
 alias rip 'expac --timefmt="%Y-%m-%d %T" "%l\t%n %v" | sort | tail -200 | nl'
-
-## Run fastfetch if session is interactive
-if status --is-interactive && type -q fastfetch
-   fastfetch --config neofetch.jsonc
-end
